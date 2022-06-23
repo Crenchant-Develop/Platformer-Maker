1606466974ab1c788d6ffc4033bec43b722f9e1a
Crenchant <levelhoon@gmail.com>
2022-06-23 18:07:29 +0900

    변경

--- Assets/Action.cs
+++ Assets/Action.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 interface IController<StateType>
 {
     StateType State { get; set; }
@@ -8,32 +7,39 @@ interface IController<StateType>
 
 interface IAction
 {
-    void Invoke<T, State>(T controller) where T : IController<State>;
+    void Invoke<T, S>(T handle) where T: class, IController<S>;
 }
-
 
 interface IAction<StateType> : IAction
 {
-    void Invoke<T>(T controller) where T : IController<StateType> 
+    // 명시적 구현 메소드
+    void IAction.Invoke<T, S>(T handle)
     {
-        Invoke<T, StateType>(controller);
+        // 형 변환에 실패할 경우 (IController<StateType>를 상속받지 않은 경우)
+        // null을 반환하고, 성공할 경우 IController<StateType>으로 형변환된 객체 값을 반환하게 된다.
+        Invoke(handle as IController<StateType>);
+
+        // 이 코드블럭이 오류가 안 나는 이유는 c# 최신 버전이기 때문이다.
+        // 구버전에서는 되지 않았었다.
     }
+
+    // 구현을 안한 추상 메소드다.
+    void Invoke(IController<StateType> controller);
 }
 
-class Jump : IAction
+class Jump : IAction<Vector2>
 {
-    public void Invoke<T1, T2>(T1 controller) where T1 : IController<T2>
+    public void Invoke(IController<Vector2> controller)
     {
-    }
-
-    public void Invoke<T>(T controller) where T: IController<Vector2>
-    {
+        Debug.Log("점프");
     }
 }
 
-class Move : IAction
+class Move : IAction<Vector2>
 {
-    public void Invoke<T, State>(T controller) where T : IController<State>
+    public void Invoke(IController<Vector2> controller)
     {
+        Debug.Log("움직인다");
     }
 }
+
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,5 +2,7 @@
 
 class PlayerController : IController<Vector2>
 {
+    Vector2 IController<Vector2>.State { set; }
+
     public Vector2 State { get; set; }
 }
